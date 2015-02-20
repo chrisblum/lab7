845d6781c74981b69c0c42c1f21b3cb6bee697a9
Chris Blum <chris.blum2@gmail.com>
2015-02-20 04:49:39 +0000

    finished

--- routes/project.js
+++ routes/project.js
@@ -20,6 +20,19 @@ exports.addProject = function(req, res) {
   var form_data = req.body;
   console.log(form_data);
 
+  var new_project = new models.Project({
+  'title' : form_data['title'],
+  'image' : form_data['image_url'],
+  'date' : form_data['date'],
+  'summary' : form_data['summary']});
+
+  new_project.save(afterSaving);
+
+  function afterSaving(err){
+    if(err){ console.log(err); res.send(500); }
+    res.redirect('/');
+  }
+
 
   // make a new Project and save it to the DB
   // YOU MUST send an OK response w/ res.send();
@@ -28,6 +41,18 @@ exports.addProject = function(req, res) {
 exports.deleteProject = function(req, res) {
   var projectID = req.params.id;
 
+      models.Project
+      .find({ "_id": projectID})
+      .remove()
+      .exec(afterRemoving)
+
+      function afterRemoving(err) {
+        if(err) {console.log(err); res.send(500);}
+        res.send();
+
+        };
+
   // find the project and remove it
   // YOU MUST send an OK response w/ res.send();
 }
+
